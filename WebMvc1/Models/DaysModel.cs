a08f3a9a0c6798c64303e5c91d36652ad332663b
KarolPetka <karolpetka1@gmail.com>
2021-11-05 00:21:48 +0100

    Init with Form1 MVC and Days MVC

---
+++ WebMvc1/Models/DaysModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebMvc1.Models
+{
+    public class DaysModel
+    {
+        [Display(Name = "Dzień urodzenia")]
+        public int Day { get; set; }
+        [Display(Name = "Miesiąc urodzenia")]
+        public int Month { get; set; }
+        [Display(Name = "Rok urodzenia")]
+        public int Year { get; set; }
+    }
+}
